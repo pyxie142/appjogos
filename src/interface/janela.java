37a82e4b5ff43eec9ac04cbe072fffd74ab926c4
pyxie142 <pietra.almeida.nunes@escola.pr.gov.br>
2025-10-01 09:54:28 -0300

    a

---
+++ src/interface/janela.java
@@ -0,0 +1,19 @@
+
+import java.awt.CardLayout;
+import java.security.Principal;
+import javax.swing.JPanel;
+
+package interface;
+
+import javax.swing.JFrame;
+
+public class janela extends JFrame {
+   public Janela() {
+    this.setBounds(0,0,1920,1080);
+    CardLayout controleTela = new CardLayout();
+    JPanel telas = new JPanel(controleTela);
+    LoginPanel LoginPanel = new LoginPanel(telas, this);
+    PrincipalPainel PrincipalPainel = new PrincipalPanel(telas, this); 
+
+   } 
+}
--- src/teste/janelateste.java
+++ src/teste/janelateste.java
@@ -1,13 +1,13 @@
 package teste;
 
-import javax.swing.JFrame;
-
 
 public class janelateste {
     public static void main(String[] args) {
-        JFrame janela = new JFrame("Titulo da janela "); 
-        janela.setBounds(0,0,1920,1080);
-        janela.setVisible(true);
+      //  JFrame janela = new JFrame("Titulo da janela "); 
+      //  janela.setBounds(0,0,1920,1080);
+       // janela.setVisible(true); 
+       Janela janela = new Janela(); 
+       
     }
 }
 
